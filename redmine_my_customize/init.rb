335fdbce3569181f375694f8c49fbb4a5e8767d8
toritori0318 <toritori0318@gmail.com>
2010-04-04 01:35:31 +0900

    ja.ymlにラベル追加

--- redmine_my_customize/init.rb
+++ redmine_my_customize/init.rb
@@ -4,6 +4,6 @@ Redmine::Plugin.register :redmine_my_customize do
   name 'Redmine My Customize plugin'
   author 'toritori0318'
   description 'This is a plugin for Redmine. additional widgets for My page.'
-  version '0.0.1'
+  version '0.0.2'
   requires_redmine :version_or_higher => '0.9.0'
 end
