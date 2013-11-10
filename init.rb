645633e12bbfc2e268dd7368f6c4586c2ea4c306
toritori0318 <toritori0318@gmail.com>
2013-11-10 20:32:38 +0900

    「プロジェクトヴィジェット」「活動ヴィジェット」追加

--- init.rb
+++ init.rb
@@ -10,6 +10,6 @@ Redmine::Plugin.register :redmine_my_customize do
   name 'Redmine My Customize plugin'
   author 'toritori0318'
   description 'This is a plugin for Redmine. additional widgets for My page.'
-  version '0.0.7'
+  version '0.0.8'
   requires_redmine :version_or_higher => '2.3.0'
 end
--- lib/redmine_my_customize/patches/my_helper.rb
+++ lib/redmine_my_customize/patches/my_helper.rb
@@ -1,6 +1,19 @@
 module RedmineMyCustomize
   module Patches
     module MyHelper
+      def sort_activity_events(events)
+        events_by_group = events.group_by(&:event_group)
+        sorted_events = []
+        events.sort {|x, y| y.event_datetime <=> x.event_datetime}.each do |event|
+          if group_events = events_by_group.delete(event.event_group)
+            group_events.sort {|x, y| y.event_datetime <=> x.event_datetime}.each_with_index do |e, i|
+              sorted_events << [e, i > 0]
+            end
+          end
+        end
+        sorted_events
+      end
+
       def my_render_project_jump_box
         @default_project = "all"
         @tmp_project = nil
